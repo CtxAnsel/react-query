03596f511bc5d10baa4b5ad4c904c1234772d53c
chentianxi <chentianxi1212@163.com>
2022-05-17 07:45:42 +0800

    feat: ReactQueryDevtools使用

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { useQuery } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools';
 import request from './request';
 
 function App() {
-  const { status, data} = useQuery('users', () => request.get('/users'));
+  const { status, data} = useQuery('users', () => request.get('/users'), {
+    // refetchOnReconnect: false,
+    // refetchOnWindowFocus: false,
+    staleTime: 5000,
+  });
   console.log(status);
   if (status === 'loading') {
     return <div>loading...</div>
@@ -10,8 +15,9 @@ function App() {
   return (
     <div className="App">
       <ul>
-        { (data || []).map(item => <li key={item.id}>{item.name}</li>) }
+        { (data || []).map(item => <li key={item.id}>{item.name + new Date()}</li>) }
       </ul>
+      <ReactQueryDevtools initialIsOpen={true}/>
     </div>
   );
 }
