adb22b0e0d261575b7158d238fd441990f0b2e12
chentianxi <chentianxi1212@163.com>
2022-05-16 22:39:10 +0800

    feat: react-query基本查询

---
+++ api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+
+const app = new express();
+app.use(express.json());
+app.use(logger('dev'));
+app.use(cors({
+  allowedHeaders: ["Content-Type"],
+  allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+}));
+
+const users = new Array(10).fill(true).map((item, index) => ({ id: String(index + 1), name: `name${index + 1}` }))
+
+app.use((req, res, next) => {
+  setTimeout(next, 1000);
+});
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.listen(8080, () => console.log("started on 8080"));
--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from "react-query";
+import request from './request';
 
 function App() {
+  const { status, data} = useQuery('users', () => request.get('/users'));
+  console.log(status);
+  if (status === 'loading') {
+    return <div>loading...</div>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        { (data || []).map(item => <li key={item.id}>{item.name}</li>) }
+      </ul>
     </div>
   );
 }
